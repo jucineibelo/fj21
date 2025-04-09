8b8afa0cf7f648e4435ae5a4c73a1984d29e1f45
Jucinei <69520602+jucineibelo@users.noreply.github.com>
2025-04-09 16:19:54 -0300

    Delete tom-cat directory

--- tom-cat/tomcat-7.0.61/bin/setenv.bat
+++
@@ -1,3 +0,0 @@
-set JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx2048m -XX:+UseConcMarkSweepGC -Djava.awt.headless=true
-set JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dmail.mime.ignoreunknownencoding=true
-set PATH=%PATH%;%CATALINA_HOME%\lib\sigar
